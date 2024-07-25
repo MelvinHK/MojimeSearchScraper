85c961dd2cc4bb0fe758c3e141a79355fcd8f329
Melvin <melvinhkusuma@gmail.com>
2024-07-25 14:45:00 +1000

    feat(scrapePage): added error handling in case animeUrl is undefined

--- src/scrapeAnimeList.js
+++ src/scrapeAnimeList.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { load } from "cheerio";
 
-import { scrapeAnimeDetails } from "./helpers.js";
+import { scrapeAnimeDetails, getLastUrlSection } from "./helpers.js";
 import { BASE_URL } from "../config.js";
 import "./types.js";
 
@@ -15,45 +15,57 @@ import "./types.js";
  * @param {number} page - The page number to start on.
  * @param {number} BATCH_THRESHOLD - The batch size limit.
  * @param {function(AnimeDetails[]): void} callback - Callback once BATCH_THRESHOLD is exceeded.
- * @param {AnimeDetails[]} [scrapedList=[]] - The currently collected items during recursion. Leave as default.
+ * @param {AnimeDetails[]} currentBatch - The currently collected items during recursion. Leave as default.
  * @returns {Promise<AnimeDetails[]>}
  */
-const scrapePage = async (page, BATCH_THRESHOLD, callback, scrapedList = []) => {
-  const response = await axios.get(`${BASE_URL}/anime-list.html?page=${page}`);
-  const $ = load(response.data);
+const scrapePage = async (page, BATCH_THRESHOLD, callback, currentBatch = []) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/anime-list.html?page=${page}`);
+    const $ = load(response.data);
 
-  const hasNextPage = $("div.anime_name.anime_list > div > div > ul > li.selected")
-    .next()
-    .length > 0;
+    const hasNextPage = $("div.anime_name.anime_list > div > div > ul > li.selected")
+      .next()
+      .length > 0;
 
-  const currentPageItems = await Promise.all(
-    $("section.content_left > div > div.anime_list_body > ul")
-      .children()
-      .map(async (_index, anime) => {
-        const animeId = $(anime).find("a").attr("href")?.split("/")[2];
-        if (animeId) {
+    const currentPageItems = await Promise.all(
+      $("section.content_left > div > div.anime_list_body > ul")
+        .children()
+        .map(async (_index, anime) => {
+          const animeUrl = $(anime).find("a").attr("href");
+          const animeId = getLastUrlSection(animeUrl); // Will throw TypeError if animeUrl is undefined.
           return await scrapeAnimeDetails(animeId);
-        }
-      })
-      .get()
-  );
+        })
+        .get()
+    );
 
-  scrapedList = scrapedList.concat(currentPageItems);
+    currentBatch = currentBatch.concat(currentPageItems);
 
-  if (scrapedList.length >= BATCH_THRESHOLD) {
-    callback(scrapedList);
-    scrapedList = [];
+    if (currentBatch.length >= BATCH_THRESHOLD) {
+      callback(currentBatch);
+      currentBatch = [];
+    }
+
+    if (hasNextPage) {
+      return scrapePage(page + 1, BATCH_THRESHOLD, callback, currentBatch);
+    }
+
+    if (currentBatch.length > 0) { // Handle remaining items on last page.
+      callback(currentBatch);
+    }
+
+    return currentBatch;
+
+  } catch (error) {
+    if (error instanceof TypeError) {
+      console.log(
+        "TypeError: animeUrl is undefined; it was unable to be scraped. " +
+        "It may be due to a network issue, or because the website layout has changed."
+      );
+    } else {
+      console.log("Error:", error);
+    }
+    return [];
   }
-
-  if (hasNextPage) {
-    return scrapePage(page + 1, BATCH_THRESHOLD, callback, scrapedList);
-  }
-
-  if (scrapedList.length > 0) { // Handle remaining items on last page.
-    callback(scrapedList);
-  }
-  
-  return scrapedList;
 };
 
 (async () => {
